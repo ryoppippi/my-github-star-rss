628b7ecae6a1c57a1d5b0b40aeb13ff87e53b20e
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-05-02 23:00:36 +0100

    fix(_ensureURL) fix for deno deploy

--- utils.ts
+++ utils.ts
@@ -1,6 +1,12 @@
 import * as U from "@core/unknownutil";
 
 export function _ensureURL(url: string) {
+  // TODO: not work in deno deploy
   // return U.ensure(URL.parse(url), U.isInstanceOf(URL));
+
+  if (!URL.canParse(url)) {
+    throw new Error("Invalid URL");
+  }
+
   return U.ensure(new URL(url), U.isInstanceOf(URL));
 }
